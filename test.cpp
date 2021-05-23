edaf7569dbf2fdf43768a1c82ddbeaff2c3eeb3b
qiao <18863889811@163.com>
2021-05-23 16:09:49 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+int main(void)
+{
+    std::cout<<"11111111111111111111"<<std::endl;
+    std::cout<<"11111111111111111111"<<std::endl;
+    std::cout<<"11111111111111111111"<<std::endl;
+    std::cout<<"11111111111111111111"<<std::endl;
+    std::cout<<"11111111111111111111"<<std::endl;
+    std::cout<<"11111111111111111111"<<std::endl;
+    std::cout<<"11111111111111111111"<<std::endl;
+    std::cout<<"11111111111111111111"<<std::endl;
+    std::cout<<"11111111111111111111"<<std::endl;
+    std::cout<<"11111111111111111111"<<std::endl;
+    std::cout<<"11111111111111111111"<<std::endl;
+    return 0;
+}
